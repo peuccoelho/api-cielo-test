e496f5c57264d4bbc6a127c61576554e2e3500ff
Pedro Coelho <150755362+peuccoelho@users.noreply.github.com>
2025-12-23 09:50:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ app.post('/create-link', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
